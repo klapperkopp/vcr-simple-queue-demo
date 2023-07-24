5a205bbad069e118eafc09474ea18075aff720e8
Toni Kuschan <toni.kuschan@vonage.com>
2023-07-24 13:59:57 +0200

    remove unescessary log

--- handleAuth.js
+++ handleAuth.js
@@ -4,7 +4,6 @@ import axios from "axios"
 export async function handleAuth(req, res, next) {
     try {
         const API_APPLICATION_ID = process.env.API_APPLICATION_ID
-        console.log(API_APPLICATION_ID)
         const response = await axios.get(`https://api.nexmo.com/v2/applications/${API_APPLICATION_ID}`, { headers: { Authorization: `${req.headers["authorization"]}` } })
         if (API_APPLICATION_ID && response?.data?.id === API_APPLICATION_ID) { next() } else throw new Error("Invalid api key and secret")
 
